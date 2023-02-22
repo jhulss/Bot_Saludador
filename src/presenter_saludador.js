a65608776420808db9cdf67ad87d7e65f3ce9bd6
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-02-21 22:04:21 -0400

    Primeras pruebas de funcionalidad

--- src/presenter_saludador.js
+++ src/presenter_saludador.js
@@ -5,8 +5,22 @@ const nombre = document.querySelector("#nombre");
 const form = document.querySelector("#saludador_form");
 const div = document.querySelector("#resultado-div");
 
+
+const genero = document.querySelector("#genero");
+const form2 = document.querySelector("#genero_form");
+const div2 = document.querySelector("#resultado-divGen");
+
+
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  genero = document.getElementById(genero);
+  if(genero.value == "Masculino")
+  {
+    div.innerHTML = "<strong>Hola </strong>"  + "Sr." + nombre.value;
+  }
+  
 
-  div.innerHTML = "<strong>Hola </strong>"  + nombre.value;
+
+
 });
