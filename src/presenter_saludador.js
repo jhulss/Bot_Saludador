768dae7878c1d08883a757d5ab08b1085288870e
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-02-22 09:18:41 -0400

    Solucion de problemas

--- src/presenter_saludador.js
+++ src/presenter_saludador.js
@@ -7,17 +7,17 @@ const div = document.querySelector("#resultado-div");
 
 
 
-const form2 = document.querySelector("#genero_form");
-const div2 = document.querySelector("#resultado-divGen");
+//const form2 = document.querySelector("#genero_form");
+//const div2 = document.querySelector("#resultado-divGen");
 
 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
     
-    if ( genero.value == "Masculino" || genero.value == "masculino" || genero.value == "hombre" || genero.value == "varon")
+    if ( genero.value === "Masculino" || genero.value === "masculino" || genero.value === "hombre" || genero.value === "varon")
     {
-        div.innerHTML = "<strong>Hola </strong>"  + "Sr." + nombre.value;
+        div.innerHTML =  "<p>" + "<strong>Hola </strong>"  + "Sr." + nombre.value + "</p>";
     }
     if (genero.value == "Femenino" || genero.value == "femenino" || genero.value == "mujer" || genero.value == "dama")
     {
