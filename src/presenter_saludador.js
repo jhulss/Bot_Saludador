d4e9f4eca6989ade91791d3ff35fe5e9b223d7ca
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-02-22 04:35:05 -0400

    Nuevos cambios realizados en presenter_saludador

--- src/presenter_saludador.js
+++ src/presenter_saludador.js
@@ -1,23 +1,27 @@
 import sumar from "./sumador";
 
 const nombre = document.querySelector("#nombre");
-
+const genero = document.querySelector("#genero");
 const form = document.querySelector("#saludador_form");
 const div = document.querySelector("#resultado-div");
 
 
-//const genero = document.querySelector("#genero");
-const form2 = document.querySelector("#genero_form");
-const div2 = document.querySelector("#resultado-divGen");
+
+//const form2 = document.querySelector("#genero_form");
+//const div2 = document.querySelector("#resultado-divGen");
 
 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  var genero = document.getElementById(generos);
-  if(genero.value == "Masculino")
-  {
-    div.innerHTML = "<strong>Hola </strong>"  + "Sr." + nombre.value;
-  }
+    
+    if ( genero.value == "Masculino" || genero.value == "masculino" || genero.value == "hombre" || genero.value == "varon")
+    {
+        div.innerHTML = "<strong>Hola </strong>"  + "Sr." + nombre.value;
+    }
+    if (genero.value == "Femenino" || genero.value == "femenino" || genero.value == "mujer" || genero.value == "dama")
+    {
+        div.innerHTML = "<strong>Hola </strong>"  + "Sra." + nombre.value;
+    }
   
 });
