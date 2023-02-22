36be04c288bddb73bf29d8da7b7cda4175ea0e4d
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-02-21 20:05:37 -0400

    Primer cambio

---
+++ src/presenter_saludador.js
@@ -0,0 +1,12 @@
+import sumar from "./sumador";
+
+const nombre = document.querySelector("#nombre");
+
+const form = document.querySelector("#saludador_form");
+const div = document.querySelector("#resultado-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  div.innerHTML = "Hola " + nombre.Value;
+});
